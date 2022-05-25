397391ae52d9c4f24bb1eacca6cc40554f29767d
[Andrii] <mirzakhanov.andrey@gmail.com>
2022-05-25 22:07:58 +0300

    Task.3

--- fastfood/accounts/views.py
+++ fastfood/accounts/views.py
@@ -1,3 +1,117 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
 
-# Create your views here.
+
+def sign_up(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/sign_up.html', context={
+            'title': 'Реєстрація'
+        })
+    elif request.method == 'POST':
+        # 1 - Отримання даних із форми реєстрації:
+        login_x = request.POST.get('login')
+        pass1_x = request.POST.get('pass1')
+        pass2_x = request.POST.get('pass2')
+        email_x = request.POST.get('email')
+
+        # 2 - Валідація даних з боку сервера:
+        color = 'gray'
+        message = 'default'
+        if pass1_x != pass2_x:
+            color = 'red'
+            message = 'Паролі не співпадають!'
+        elif pass1_x == '':
+            pass
+        else:
+            # 3 - Додавання користувача в базу даних:
+            user = User.objects.create_user(login_x, email_x, pass1_x)
+
+            # 4 - формування звіту:
+            if user is None:
+                color = 'red'
+                message = 'В регістрації відмовлено!'
+            else:
+                user.save()
+                color = 'green'
+                message = 'Регістрацію успішно завершено'
+        # 6 - Завантаження сторінки звіту:
+        return render(request, 'home/index.html', context={
+            'title': 'Звіт про реєстрацію',
+            'color': color,
+            'message': message
+        })
+
+
+def sign_in(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/sign_in.html', context={
+            'title': 'Авторизація'
+        })
+    elif request.method == 'POST':
+        # 1 - Зчитаємо дані із форми:
+        email_x = request.POST.get('email')
+        pass_x = request.POST.get('pass')
+
+        # 1.1. Получаем юзера
+        user_x = get_user(email_x)
+        if user_x is None:
+            color = 'red'
+            message = 'Користувач не знайдений'
+            title = 'Звіт про авторизацію'
+            return render(request, 'accounts/report.html', context={
+                'color': color,
+                'message': message,
+                'title': title
+            })
+
+        # 2 - Перевіряємо наявність запису у БД:
+        user = authenticate(request, username=user_x, password=pass_x)
+        if user is not None:
+            login(request, user)
+            return render(request, 'home/index.html')
+        else:
+            color = 'red'
+            message = 'Користувач не знайдений'
+            title = 'Звіт про авторизацію'
+            return render(request, 'accounts/report.html', context={
+                'color': color,
+                'message': message,
+                'title': title
+            })
+
+
+def sign_out(request):
+    logout(request)
+    return render(request, 'home/index.html', context={
+        'title': 'Вихід із системи'
+    })
+
+
+def profile(request):
+    return render(request, 'home/index.html', context={
+        'title': 'Профіль користувача'
+    })
+
+
+def ajax_email(request):
+    email_y = request.GET.get('email')
+
+    try:
+        User.objects.get(email=email_y)
+        message = 'зайнятий'
+    except User.DoesNotExist:
+        message = 'вільний'
+    return JsonResponse({
+        'message': message
+    })
+
+
+def get_user(email):
+    user = User.objects.get(email=email)
+    if user is None:
+        return None
+    else:
+        return user
+
---
+++ fastfood/static/js/sign_up.js
@@ -0,0 +1,76 @@
+$(document).ready(() => {
+
+   console.log('jQuery-OK');
+   let validateResult1 = false;  //- результат валідації 1 пароля
+   let validateResult2 = false;  //- результат валідації 2 пароля
+   let validateResult3 = false;  //- результат валідації email
+
+   // 3. Валідація 1 пароля:
+   // ----------------------
+   $('#pass1').blur(() => {
+        let pass1X = $('#pass1').val();
+        let passRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_\-])[a-zA-Z0-9_\-]{8,}$/;
+
+        console.log(pass1X);
+        if (passRe.test(pass1X)) {
+            $('#pass1_err').text('');
+            validateResult1 = true;
+        } else {
+            $('#pass1_err').text('Пароль має містити хоча б одну головну букву, цифру та спеціальний знак');
+            validateResult1 = false;
+        }
+   });
+
+   // 4. Валідація 2 пароля:
+   // ----------------------
+   $('#pass2').blur(() => {
+        let pass1X = $('#pass1').val();
+        let pass2X = $('#pass2').val();
+
+        console.log(pass1X + ' / ' + pass2X);
+        if (pass1X === pass2X) {
+            $('#pass2_err').text('');
+            validateResult2 = true;
+        } else {
+            $('#pass2_err').text('Введені паролі не співпадають');
+            validateResult2 = false;
+        }
+   });
+
+   // 5. Валідація e-mail
+   // -------------------
+   $('#email').blur(() => {
+        let emailX = $('#email').val();
+        let emailRe = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+
+        console.log(emailX);
+        if (emailRe.test(emailX)) {
+            $.ajax({
+                url: '/accounts/ajax_email',
+                data: 'email=' + emailX,
+                success: (result) => {
+                    if (result.message === 'зайнятий') {
+                     $('#email_err').text('E-Mail - зайнятий!  Спробуйтк інший');
+                     validateResult3 = false;
+                    } else {
+                        $('#email_err').text('');
+                        validateResult3 = true;
+                    }
+                }
+            });
+        } else {
+            $('#email_err').text('Введіть коректний E-Mail');
+            validateResult3 = false;
+        }
+   });
+
+
+   $('#submit').click(() => {
+        if (validateResult1 === true && validateResult2 === true && validateResult3 === true) {
+            $('#form-1').attr('onsubmit', 'return true');
+        } else {
+            $('#form-1').attr('onsubmit', 'return false');
+            alert('Форма містить некоректні дані!\nВідправка даних заблокована!');
+        }
+   })
+});
